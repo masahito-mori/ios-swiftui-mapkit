bb4cb998bdbe8ea4cf5cbc30d532956b7d972da6
fore447 <mmori.0426@gmail.com>
2023-10-27 19:02:56 +0900

    ♻️[地図画面]位置情報のCLAuthorizationStatusに関わる処理をリファクタ
    【対応内容】
    ・locationManagerDidChangeAuthorizationはLocationManagerのインスタンスを生成した時にも呼ばれるので、initで行っていたCLAuthorizationStatusの判定処理を削除した。
    ・https://developer.apple.com/documentation/corelocation/cllocationmanagerdelegate/3563956-locationmanagerdidchangeauthoriz
    
    【影響範囲】
    ・地図画面
    
    【動作確認】
    ・初回画面表示時も位置情報許可ダイアログが表示される
    
    # ==== Emojis ====
    # 🐝 バグ修正
    # 👍 機能改善
    # ✨ 部分的な機能追加
    # 🎨 デザイン変更のみ
    # 💢 コンフリクト
    # 🚧 WIP
    # 📝 文言修正
    # ♻️ リファクタリング
    # 🔥 不要な機能・使われなくなった機能の削除
    # 💚 テストやCIの修正・改善
    # 👕 Lintエラーの修正やコードスタイルの修正
    # 🚀 パフォーマンス改善
    # 📂 ファイル追加
    # 🆙 依存パッケージなどのアップデート
    # 👮 セキュリティ関連の改善
    # ⚙  config変更
    # 📚 ドキュメント

--- MapKitForSwiftUI/LocationManager.swift
+++ MapKitForSwiftUI/LocationManager.swift
@@ -18,14 +18,19 @@ final class LocationManager: NSObject, ObservableObject {
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         // 5m移動したら、位置情報を通知
         locationManager.distanceFilter = 5.0
-        setup()
     }
 }
 
 extension LocationManager: CLLocationManagerDelegate {
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-        guard manager.authorizationStatus == .authorizedWhenInUse else { return }
-        locationManager.requestLocation()
+        switch manager.authorizationStatus {
+            case .notDetermined:
+                locationManager.requestWhenInUseAuthorization()
+            case .authorizedWhenInUse, .authorizedAlways:
+                locationManager.requestLocation()
+            default:
+                break
+        }
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
@@ -38,20 +43,6 @@ extension LocationManager: CLLocationManagerDelegate {
                 center: location.coordinate,
                 span: .init(latitudeDelta: 0.01, longitudeDelta: 0.01)
             )
-        }
-    }
-}
-
-private extension LocationManager {
-    func setup() {
-        switch locationManager.authorizationStatus {
-        case .authorizedWhenInUse:
-          locationManager.requestLocation()
-        case .notDetermined:
-            //ユーザーが位置情報の許可をまだしていないので、位置情報許可のダイアログを表示する
-            locationManager.requestWhenInUseAuthorization()
-        default:
-          break
         }
     }
 }
